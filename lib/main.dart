3cd542b344526f3ec016a6e30ab0cc29c4ad107e
ehabatef205 <ehabmahmoud205@gmail.com>
2023-07-04 23:20:22 +0300

    connection with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:englizy_app/MyBlocObserver.dart';
 import 'package:englizy_app/modules/logIn/logIn_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'layout/cubit/cubit.dart';
 import 'layout/cubit/states.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   return runApp(const MyApp());
 }
